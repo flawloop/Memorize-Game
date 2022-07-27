a39748014cc218ef07cd000ff3e2dcdc0f0626d2
flawloop <khusravkhamidov@gmail.com>
2022-07-28 00:45:07 +0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by yaaburnee on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
